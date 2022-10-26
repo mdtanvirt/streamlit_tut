f39deeb1dd4128f0e06cd0975f07cbd2cf013d52
Tanvir Rahman <trahman@paradynamix.com>
2022-10-26 18:01:07 +0600

    Updated app file

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title("Hello Tanvir")
 st.header("Header")
 st.subheader("Sub-Header this is test")
+st.text("This is a text for streamlit dash board details")
+
+st.markdown(""" # h1 tag
+## h2 tag
+### h3 tag
+""")
