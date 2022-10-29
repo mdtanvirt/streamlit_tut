e0bdb41173322a4041f1674a821fec6530d718a0
Tanvir Rahman <trahman@paradynamix.com>
2022-10-29 13:49:53 +0600

    Home page with filtering options

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from matplotlib import  pyplot as plt
+from plotly import graph_objs as go
 
 # Hide streamlit default menu and footer from the template
 hide_st_style = """
@@ -23,6 +24,9 @@ if nav == "Home":
         st.table(data)
 
     grapg = st.selectbox("What kind of Graph?", ["Non-Interactive", "Interactive"])
+
+    val = st.slider("Filter data using year of experience", 0,20)
+    data = data.loc[data["YearsExperience"] >= val]
     
     if grapg == "Non-Interactive":
         plt.figure(figsize=(10, 5))
@@ -34,7 +38,12 @@ if nav == "Home":
         st.pyplot()
 
     if grapg == "Interactive":
-        pass
+        layout = go.Layout(
+            xaxis = dict(range=[0,16]),
+            yaxis = dict(range=[0, 210000])
+        )
+        fig = go.Figure(data=go.Scatter(x=data["YearsExperience"], y=data["Salary"], mode="markers"), layout=layout)
+        st.plotly_chart(fig)
 
 if nav == "Prediction":
     st.write("Prediction")
